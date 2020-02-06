0bc444171692dc0deaa92c87e3aff7a0967120b3
Selvamanikandan K <selvamanikandan.kalirajan@global.chainsys.com>
2020-02-06 18:20:04 +0530

    removed java

--- src/main/java/com/chainsys/PayrollApp/dao/AccountantDAO.java
+++
@@ -1,9 +0,0 @@
-package com.chainsys.PayrollApp.dao;
-
-public interface AccountantDAO 
-{
-	String calculatePF() throws Exception ;
-	String calculateIncrement() throws Exception ;
-	String calculatesalary() throws Exception ;
-	String markAttendance() throws Exception ;
-}
